19a0c4ac8341682ac153357cc99b719f8065ae4e
maeva pham <maevapham.contact@gmail.com>
2019-05-28 18:59:35 +0200

    controller plants

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'plants/index'
-  get 'plants/show'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
