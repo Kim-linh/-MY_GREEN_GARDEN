5916a29fc3381767d12920c1fac33b03b2a95e7a
caroline faugas <cfaugas@hotmail.com>
2019-05-29 16:00:02 +0200

    creation page new create de userplant et modif css

--- app/models/user_plant.rb
+++ app/models/user_plant.rb
@@ -2,9 +2,9 @@ class UserPlant < ApplicationRecord
   belongs_to :user
   belongs_to :plant
   has_many :activities
-  validates :name, presence:true
-  validates :origin_health, presence:true
-  enum origin_health: { awful:0, bad:1, OK:2, good:3, excellent:4 }
+  validates :name, presence: true
+  validates :origin_health, presence: true
+  enum origin_health: { awful: 0, bad: 1, ok: 2, good: 3, excellent: 4 }
 
   #delegate :variety, :description, to: :plant
 
