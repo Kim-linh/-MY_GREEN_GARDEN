0ad8406d02e68e0175d72b6d5f06ae8e2e94f26e
Kurt Pascual <kurt.pascual84@gmail.com>
2019-05-30 15:13:49 +0200

    suppression de la colone date

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,6 +1,5 @@
 class Activity < ApplicationRecord
   belongs_to :user_plant
-  validates :date, presence: true
   validates :health, presence: true
   enum health: { awful: 0, bad: 1, OK: 2, good: 3, excellent: 4 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_29_144117) do
+ActiveRecord::Schema.define(version: 2019_05_30_130001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "activities", force: :cascade do |t|
     t.bigint "user_plant_id"
-    t.datetime "date"
     t.integer "health"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
