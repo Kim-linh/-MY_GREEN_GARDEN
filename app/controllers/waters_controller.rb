3bcf91cda3c00c3353e2cf6e7660e051628bf52b
Kurt Pascual <kurt.pascual84@gmail.com>
2019-05-30 18:52:59 +0200

    fixcountdown

--- app/controllers/waters_controller.rb
+++ app/controllers/waters_controller.rb
@@ -4,19 +4,5 @@ class WatersController < ApplicationController
   def create
     @user_plant = UserPlant.find(params[:user_plant_id])
     @water = Water.create(user_plant: @user_plant)
-raise
-    # @countdown = userplant.start_countdown
-    # date1 = Time.now
-    # puts date1
-    # while Time.now < date1
-    #   t = Time.at(date1.to_i - Time.now.to_i)
-    #   puts t.strftime('%M:%S')
-    #   sleep 1
-    end
   end
-
-    private
-
-  def activity_params
-    params.require(:waters).permit(:user_plant_id)
-  end
+end
--- app/models/water.rb
+++ app/models/water.rb
@@ -1,3 +1,12 @@
 class Water < ApplicationRecord
   belongs_to :user_plant
+
+  def countdown
+    dateX = self.created_at + 10.days
+    date_seconds = dateX - DateTime.now
+    date_to_date = date_seconds / (60 * 60 * 24)
+    days = date_to_date.round
+    # hours_to_date = (date_to_date - days) * 24
+    # hours_to_date.round
+  end
 end
