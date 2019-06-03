952fbfdc7cd64b5f9d8efe0c8ae33fb9472b9dde
caroline faugas <cfaugas@hotmail.com>
2019-06-03 15:10:16 +0200

    front home + geomap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,11 @@
 //= require rails-ujs
 //= require_tree .
+navigator.geolocation.getCurrentPosition(function(location) {
+ var lat = location.coords.latitude;
+ var lng = location.coords.longitude;
+ var latlng = lat + "," + lng;
+ var url = `https://www.google.com/maps/embed/v1/search?key=AIzaSyDGE74FeKy-wiLTRwCOOdSj0UEa5kmPhO0&q=fleuristes&zoom=14&center=${latlng}`
+ var iframe = document.querySelector("#florist-map");
+ var src = iframe.getAttribute('src');
+ iframe.setAttribute("src", url);
+});
