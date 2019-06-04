a0ffb452c731a07d22b1fa53611cd8e8052ca814
caroline faugas <cfaugas@hotmail.com>
2019-06-04 15:20:12 +0200

    change config

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module MyGreenGarden
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
     config.generators do |generate|
           generate.assets false
           generate.helper false
@@ -15,6 +16,7 @@ module MyGreenGarden
         end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
