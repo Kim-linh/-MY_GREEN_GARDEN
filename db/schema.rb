a1784b26fadf0ed05cfe71b8ee692c1ea4452852
caroline faugas <cfaugas@hotmail.com>
2019-06-04 16:15:33 +0200

    clean migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_04_102707) do
+ActiveRecord::Schema.define(version: 2019_06_04_132524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,7 +59,7 @@ ActiveRecord::Schema.define(version: 2019_06_04_102707) do
     t.string "last_name"
     t.string "avatar"
     t.string "address"
-    t.integer "number"
+    t.string "number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
