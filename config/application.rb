4a7820045b8091a01ce67b35c16145b8195f94a9
maeva pham <maevapham.contact@gmail.com>
2019-06-06 18:14:34 +0200

    mois en francais

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,13 @@ module MyGreenGarden
           generate.test_framework  :test_unit, fixture: false
         end
     # Initialize configuration defaults for originally generated Rails version.
+
     config.load_defaults 5.2
+
+
+    config.i18n.default_locale = :fr
+    config.i18n.available_locales = [:en, :fr]
+
 
 
     # Settings in config/environments/* take precedence over those specified here.
